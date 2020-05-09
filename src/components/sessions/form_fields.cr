49d933dba9410265e4c27581f77ab1a88ce7c110
Jade D. Kharats <jade.kharats@gmail.com>
2020-05-09 10:43:13 +0200

    :recycle: form date_input

--- src/components/sessions/form_fields.cr
+++ src/components/sessions/form_fields.cr
@@ -4,18 +4,8 @@ class Sessions::FormFields < BaseComponent
   def render
 
     mount Shared::Field.new(operation.formateur), &.text_input(autofocus: "true")
-    render_date_field(operation.begin_date,"Date de début" )
-    render_date_field(operation.end_date,"Date de fin" )
+    mount Shared::Field.new(operation.begin_date, "Date de début"), &.date_input
+    mount Shared::Field.new(operation.end_date, "Date de fin"), &.date_input
     mount Shared::Field.new(operation.intitule, "Intitulé")
-  end
-
-  private def render_date_field(attribute, label_text)
-    div class: "field" do
-      label_for attribute, label_text, class: "label"
-      div class: "control" do
-        date_input(attribute, attrs: [:required], class: "input" )
-        mount Shared::FieldErrors.new(attribute)
-      end
-    end
   end
 end
